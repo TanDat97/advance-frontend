f01703e85adb67700adcc3bd84920b2ce1311c1d
Tran Tan Dat <trantandat130497@gmail.com>
2020-06-28 21:13:26 +0700

    custom video player, exchange rate

--- 1-movie-seat/script.js
+++ 1-movie-seat/script.js
@@ -32,9 +32,9 @@ function updateSelectedCount() {
 function populateUI() {
   const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
 
-  if(selectedSeats !== null && selectedSeats.length > 0) {
+  if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
-      if(selectedSeats.indexOf(index) > -1) {
+      if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add('selected');
       }
     });
